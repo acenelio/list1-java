55c1f0fecd213019fd756f28a795e0c5eb909657
fredbenevides <44215428+fredbenevides@users.noreply.github.com>
2019-02-16 00:51:07 -0500

    Remove the setter for salary

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -34,10 +34,6 @@ public class Employee {
 	public Double getSalary() {
 		return salary;
 	}
-
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
 	
 	public void increaseSalary(double percentage) {
 		salary += salary * percentage / 100.0;
