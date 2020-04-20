ecfdfb953b51f42a61bf64db88f2dab952c9e4cf
Nelio Alves <acenelio@gmail.com>
2020-04-20 00:30:36 -0300

    Including id validation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,8 +24,14 @@ public class Program {
 		for (int i=1; i<=n; i++) {
 			System.out.println();
 			System.out.println("Employee #" + i + ": ");
+
 			System.out.print("Id: ");
 			int id = sc.nextInt();
+			while (hasId(list, id)) {
+				System.out.print("Id already taken. Try again: ");
+				id = sc.nextInt();
+			}
+			
 			System.out.print("Name: ");
 			sc.nextLine();
 			String name = sc.nextLine();
@@ -58,5 +64,10 @@ public class Program {
 		}
 				
 		sc.close(); 
+	}
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
 	}
 }
